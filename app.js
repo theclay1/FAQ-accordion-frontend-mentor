8307d1209c8742e6cb502c6937a0fee6688aa515
amr muhammad <88667922+theclay1@users.noreply.github.com>
2024-02-02 00:51:47 +0200

    fix img src 0.4

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 let plus = document.querySelectorAll(".Q>img");
 
 document.querySelector("main").addEventListener("click", (ele) => {
-  if (ele.target.src === "img/icon-plus.svg") {
+  if (ele.target.src === "./img/icon-plus.svg") {
     for (i = 0; i < plus.length; i++) {
       plus[i].parentElement.parentElement.lastElementChild.style.display =
         "none";
-      plus[i].src = "img/icon-plus.svg";
+      plus[i].src = "./img/icon-plus.svg";
     }
     ele.target.parentElement.parentElement.lastElementChild.style.display =
       "block";
-    ele.target.src = "img/icon-minus.svg";
-  } else if (ele.target.src === "img/icon-minus.svg") {
+    ele.target.src = "./img/icon-minus.svg";
+  } else if (ele.target.src === "./img/icon-minus.svg") {
     ele.target.parentElement.parentElement.lastElementChild.style.display =
       "none";
-    ele.target.src = "img/icon-plus.svg";
+    ele.target.src = "./img/icon-plus.svg";
   }
 });
